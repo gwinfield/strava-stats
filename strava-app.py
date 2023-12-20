7a82216dacdc182af05bec520120c62da7241b08
gwinfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 10:25:09 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("2023 Strava Statistics:bike:")
 
-file = st.file_uploader("Upload your Strava data is a .csv format!", key="loader", type="csv")
+file = st.file_uploader("Upload your Strava data is a .csv format!", key="loader", type='csv')
 
 if file != None:
   df = pd.read_csv(file)
