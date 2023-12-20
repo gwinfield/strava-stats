5fca88f76c3ecea503e3a36fe9b9e6c235ffa61a
gwinfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 10:39:01 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -10,8 +10,9 @@ st.title("2023 Strava Statistics:athletic_shoe:")
 st.write("Welcome to (an unofficial form of) Strava Wrapped for 2023! Please enjoy your personalized dashboard.")
 
 st.subheader("File Upload")
+st.write("Click here to learn how to download your data.\nOnce you've downloaded gthe zip file, download the "activities.csv" file.")
 
-file = st.file_uploader("Upload your Strava data is a .csv format!", key="loader", type='csv')
+file = st.file_uploader("Upload your Strava data!", key="loader", type='csv')
 
 if file != None:
   df = pd.read_csv(file)
