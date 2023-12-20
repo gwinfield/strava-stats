11f6b8f65474b37151de34be6fee9c77a5d9e9e8
gwinfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 10:23:53 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -3,7 +3,13 @@ import pandas as pd
 
 st.title("2023 Strava Statistics:bike:")
 
-[theme]
-primaryColor = '#fc4c02'
-backgroundColor = '#ffffff'
-textColor = '#OOOO8O'
+file = st.fileuploader("Upload your Strava data is a .csv format!", key="loader", type="csv")
+
+if file != None:
+  df = pd.read_csv(file)
+  st.write(df)
+
+#[theme]
+#primaryColor = '#fc4c02'
+#backgroundColor = '#ffffff'
+#textColor = '#OOOO8O'
