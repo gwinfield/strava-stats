255f5591f2d3dd299b8e02198ae3e2babe8b3b0a
gwinfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 10:50:43 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -8,14 +8,14 @@ st.set_page_config(
 )
 
 st.title("2023 Strava Statistics:athletic_shoe:")
-st.text("Welcome to (an unofficial form of) Strava Wrapped for 2023! Please enjoy your personalized dashboard.")
+st.write("Welcome to (an unofficial form of) Strava Wrapped for 2023! Please enjoy your personalized dashboard.")
 
 st.subheader("Upload your actuvity data here!")
 
 url = "https://support.strava.com/hc/en-us/articles/216918437-Exporting-your-Data-and-Bulk-Export"
 st.markdown("Click [here](%s) to learn how to download your data." % url)
 
-st.text("Once you've downloaded gthe zip file, download the activities.csv file.")
+st.write("Once you've downloaded gthe zip file, download the activities.csv file.")
 
 file = st.file_uploader("Drop your activities file in a csv format", key="loader", type='csv')
 
