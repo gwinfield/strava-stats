1948116cb2c31b8c3590fb8512119f9d93db5937
gwinfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 12:43:57 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -17,13 +17,12 @@ url = "https://support.strava.com/hc/en-us/articles/216918437-Exporting-your-Dat
 message = st.markdown("Click [here](%s) to learn how to download your data.\nOnce you've downloaded the zip file, download the activities.csv file." % url)
 
 file = st.file_uploader(message, key="loader", type='csv')
+original_activities = pd.read_csv(file)
+  #st.write(original_activities)
 
 if file != None:
   st.markdown("""---""")
   st.subheader("See your stats!")
-
-  original_activities = pd.read_csv(file)
-  #st.write(original_activities)
 
   def km_to_mi(km):
     return km*0.621371
