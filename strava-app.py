febd8c79b802c220e61fd9b1375a47c34d9da164
Georgia Winfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 21:10:25 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -78,9 +78,7 @@ if file != None:
   with tab1:
     st.header("Count by Month")
     month_counts = activities_filtered['Month'].value_counts()
-    #months_categories = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-    #month_counts['Months'] = pd.Categorical(month_counts['Months'], categories = months_categories)
-    #month_counts.sort_values(by="Month", inplace=True)
+    month_counts.sort_values(by="count")
     st.bar_chart(month_counts, color=["#fc4c02"])
      
   with tab2:
@@ -96,7 +94,10 @@ else:
    
     #pass
 
- #month = st.sidebar.multiselect("Month:", options = activities["Month"].unique(), default = activities["Month"].unique())
+#months_categories = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    #month_counts['Months'] = pd.Categorical(month_counts['Months'], categories = months_categories)
+
+#month = st.sidebar.multiselect("Month:", options = activities["Month"].unique(), default = activities["Month"].unique())
   #dotw = st.sidebar.multiselect("Day of the Week:", options = activities["Day of the Week"].unique(), default = activities["Day of the Week"].unique())
 
 # def create_tabs(data):
