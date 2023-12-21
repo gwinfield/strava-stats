fe0887e40275ca2f617ba5defedf16a0c2f91668
Georgia Winfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 19:35:01 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -68,8 +68,21 @@ if file != None:
 
   activities = transform_data(original_activities)
 
-  with st.sidebar:
-    st.write("TEST")
+  tab1, tab2, tab3 = st.tabs(["Bar Graph", "Statistics", "Data"])
+
+  with tab1:
+    st.header("Count of Activity Types")
+    activity_counts = activities['Activity Type'].value_counts()
+    st.bar_chart(activity_counts)
+    with st.sidebar:
+      st.write("TEST")
+     
+  with tab2:
+    st.header("Stats")
+
+  with tab3:
+    st.header("Data Preview")
+    st.write(activities.head(15))
     
 else:
   pass
