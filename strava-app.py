19e2f62c26fa2d934bca4fead59093fe6cdd9170
Georgia Winfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 19:17:43 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -74,7 +74,7 @@ if file != None:
     for tab in st.tabs(activities_list):
       with tab:
         filtered_activities = {}
-        filtered_activities[tab] = data.loc[data['Activity Type'] == tab].copy())
+        filtered_activities[tab] = data.loc[data['Activity Type'] == tab].copy()
         st.dataframe(data=filtered_activities[tab])
    
     pass
