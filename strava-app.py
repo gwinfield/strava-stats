02a8447648fd5ab859a1c5e9938499248ede35c1
Georgia Winfield <154380085+gwinfield@users.noreply.github.com>
2023-12-21 01:22:57 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -87,7 +87,7 @@ if file != None:
 
   def total_time(data):
     data["Elapsed Time"] = data["Elapsed Time"]
-    total_time = data["Elapsed Time"].sum() #in min
+    total_time = data["Elapsed Time"].sum()*60 #in min
     return st.write(f"Total Time: {round(total_time, 2)} hours")
 
   def time_per_month_graph(data):
