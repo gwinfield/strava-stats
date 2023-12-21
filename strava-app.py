3c09ef9ad39a844e97ea8eb70d6825df14b2967d
Georgia Winfield <154380085+gwinfield@users.noreply.github.com>
2023-12-20 21:03:02 -0600

    Update strava-app.py

--- strava-app.py
+++ strava-app.py
@@ -79,6 +79,8 @@ if file != None:
   with tab1:
     st.header("Count by Month")
     month_counts = activities_filtered['Month'].value_counts()
+    months_categories = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    month_counts['Months'] = month_counts.Categorical(month_counts['Months'], categories = months_categories)
     month_counts.sort_values(by=['Month'])
     st.bar_chart(month_counts, color=["#fc4c02"])
      
